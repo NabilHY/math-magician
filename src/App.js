32485aa2800c66f143545d980acf4742761b7c79
nabsternab <hayounn.etd@gmail.com>
2022-05-25 13:19:24 +0100

    Refactor component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import './App.css';
 import Calculator from './component/calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends React.PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
