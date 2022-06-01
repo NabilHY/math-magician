dc976f53ee0e26b260f44cf7cd2faf9fc49baa38
nabsternab <hayounn.etd@gmail.com>
2022-06-01 19:33:23 +0100

    Fix Linters

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React from 'react';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,5 +1,4 @@
 import { NavLink } from 'react-router-dom';
-import Divider from '@material-ui/core/Divider';
 import './Navbar.module.css';
 
 const NavBar = () => (
@@ -7,9 +6,7 @@ const NavBar = () => (
     <h1>Math Magicien</h1>
     <div className="link-items">
       <NavLink to="/">Home</NavLink>
-      <Divider vertical />
       <NavLink to="/calculator">Calculator</NavLink>
-      <Divider className="divider" vertical />
       <NavLink to="/quote">Quote</NavLink>
     </div>
   </nav>
