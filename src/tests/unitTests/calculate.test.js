7e6c15718a6adbe8556771bb7da7768d58a224e8
tahenyBELGUITH <taheny.belguith@gmail.com>
2022-06-10 11:34:07 +0100

    Add the tset for calculate function

---
+++ src/tests/unitTests/calculate.test.js
@@ -0,0 +1,11 @@
+import calculate from "../../logic/calculate";
+describe("calculate function works correctely", () => {
+  test("calculate returns object", () => {
+    const fnc = calculate({}, "AC");
+    expect(fnc).toEqual({
+      total: null,
+      next: null,
+      operation: null,
+    });
+  });
+});
