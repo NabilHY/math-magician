fc99c7b808cd5decf889ceb8f5c98aa2ea7ba317
GASOMINALI Olivier <ogasominali@gmail.com>
2022-06-10 10:11:18 +0200

    Update Quote.test.js

--- src/tests/unitTests/Quote.test.js
+++ src/tests/unitTests/Quote.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Quote from '../Quote';
+import Quote from './component/quote';
 
 it('matches snapshot', () => {
   const tree = renderer.create(
