37d11e35413041a9103a4a3a14f2c64a8d1a6f55
nabsternab <hayounn.etd@gmail.com>
2022-06-10 11:22:52 +0100

    Create snapshot tests

--- src/tests/snapshots/QuoteSnapshot.test.js
+++
@@ -1,9 +0,0 @@
-import renderer from 'react-test-renderer';
-import Quote from '../../component/quote';
-
-describe('Quote component renders correctely', () => {
-  it('Quote page matches snapshot', () => {
-    const tree = renderer.create(<Quote />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-});
